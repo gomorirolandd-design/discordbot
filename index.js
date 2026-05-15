c1a6694002a2abdcd05f03f068559a3a1c53d6b0
Roland <gomori.rolandd@gmail.com>
2026-05-15 13:13:27 +0200

    final port fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Bot működik!");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Webserver elindult");
 });
 require('dotenv').config();
