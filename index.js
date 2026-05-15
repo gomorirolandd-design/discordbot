498ee79d48c87968b251c92b34fc17d256c89158
Roland <gomori.rolandd@gmail.com>
2026-05-15 13:05:46 +0200

    express

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot működik!");
+});
+
+app.listen(3000, () => {
+  console.log("Webserver elindult");
+});
 require('dotenv').config();
 
 const {
